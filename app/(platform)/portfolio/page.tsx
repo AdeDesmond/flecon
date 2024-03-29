8ec0c25ac0502a2db63da2de0ab2f0d5122f6d58
DesmondAde1993 <140061312+AdeDesmond@users.noreply.github.com>
2024-03-30 07:01:18 +0800

    Feat:min-height added to all pages

--- app/(platform)/portfolio/page.tsx
+++ app/(platform)/portfolio/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function PortFolioPage() {
   return (
-    <main className={cn("overflow-y-scroll w-full")}>
+    <main className={cn("overflow-y-scroll w-full min-h-screen")}>
       this will be the portfolio page
     </main>
   );
--- app/(platform)/service/page.tsx
+++ app/(platform)/service/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { cn } from "@/lib/utils";
 function page() {
   return (
-    <main className={cn("overflow-y-scroll w-full")}>
+    <main className={cn("overflow-y-scroll w-full min-h-screen")}>
       this will be the services page
     </main>
   );
