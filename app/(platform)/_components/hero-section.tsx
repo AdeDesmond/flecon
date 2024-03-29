08b7701c708c2d5417120aa0ddf8972d1c88f22a
DesmondAde1993 <140061312+AdeDesmond@users.noreply.github.com>
2024-03-29 14:56:54 +0800

    Feat: initial styles

--- app/(platform)/_components/hero-section.tsx
+++ app/(platform)/_components/hero-section.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, BarChart4Icon, AreaChartIcon } from "lucide-react";
 import localfont from "next/font/local";
 import Image from "next/image";
 
@@ -44,14 +44,22 @@ export const HeroSection = () => {
             className="object-cover"
           />
         </div>
-        <div className="bg-[#12182B] h-[6rem] w-[9rem] absolute top-10 right-[-3rem] rounded text-white z-[100] hidden lg:block">
-          some
+        <div className="bg-[#12182B] h-[6rem] w-[9rem] absolute top-10 right-[-3rem] rounded text-white z-[100] hidden lg:flex lg:items-center lg:flex-col lg:justify-center">
+          <span className="text-xs">+80.5% Increase sales</span>
+          <AreaChartIcon className="w-[3rem] h-[3rem] text-[#69fab4]" />
         </div>
-        <div className="bg-[#12182B] h-[6rem] w-[11rem] absolute top-[10rem] left-[-4rem] rounded text-white z-[100] hidden lg:block">
-          things
+        <div className="bg-[#12182B] h-[6rem] w-[11rem] absolute top-[10rem] left-[-4rem] rounded text-white z-[100] hidden lg:flex lg:items-center lg:justify-center lg:gap-3 lg:pl-4 p-2">
+          <BarChart4Icon className="w-[5rem] h-[5rem] text-[#69fab4]" />
+          <p className="text-xs">
+            Our <br /> progressive minds
+          </p>
         </div>
-        <div className="bg-[#12182B] h-[4rem] w-[15rem] absolute bottom-10 right-[-3rem] rounded text-white z-[100] hidden lg:block">
-          service
+        <div className="bg-[#12182B] h-[4rem] w-[15rem] absolute bottom-10 right-[-3rem] rounded text-white z-[100] hidden lg:block p-1">
+          <span className="text-xs">+70%</span>
+          <div className="relative block w-full rounded-md bg-[#ffffff] h-2">
+            <span className="absolute top-0 left-0 bg-[#69fab4] w-[80%] h-2 rounded-l-md"></span>
+          </div>
+          <span className="text-xs">Increase sales</span>
         </div>
 
         <Image
